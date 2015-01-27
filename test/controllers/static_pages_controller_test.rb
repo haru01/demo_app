1db0f93de8d9f30c797b12623f7b2387853094da
eiji.ienaga <eiji.ienaga@gmail.com>
2015-01-27 17:09:05 +0900

    重複をapplicationに共通かした

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,11 +5,13 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :home
     assert_response :success
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "h1", "Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "h1", "Help"
   end
 end
